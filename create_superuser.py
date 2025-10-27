a794a51fe70b87c3925fbafd05faae2e3ba0ff96
Tafsir <mba898127@gmail.com>
2025-10-27 11:18:48 +0100

    superuser

--- create_superuser.py
+++ create_superuser.py
@@ -9,8 +9,8 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin@example.com", "MotDePasse123")
+if not User.objects.filter(username="Admin").exists():
+    User.objects.create_superuser("Admin", "mba898127@gmail.com", "@lkr33m66")
     print("Superuser créé !")
 else:
     print("Le superuser existe déjà.")
